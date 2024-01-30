c349fa170cab885c442a06af169a1107792b5663
BatLeDev <baptiste.guerny@gmail.com>
2024-01-30 09:14:47 +0100

    feat: Editable dataset
    - Generate the schema
    - Create the dataset
    - API call
    - Send data after each page

--- index.js
+++ index.js
@@ -1,16 +1,125 @@
-const fs = require('fs-extra')
-const path = require('path')
-const process = require('./lib/process')
-const upload = require('./lib/upload')
+// Get the value of an object by a "path" string
+// obj: a javascript object like { a: { b: { c: 1 } } }
+// path: a string like "a.b.c"
+function getValueByPath (obj, path) {
+  const keys = path.split('.')
+  for (const key of keys) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      obj = obj[key]
+    } else {
+      return null
+    }
+  }
+  return obj
+}
 
 exports.run = async ({ pluginConfig, processingConfig, processingId, tmpDir, axios, log, patchConfig }) => {
-  const tmpFile = path.join(tmpDir, 'transformed.csv')
-  await fs.ensureDir(tmpDir)
+  await log.step('Initialisation')
 
-  await process(processingConfig, tmpFile, axios, log)
-  await upload(processingConfig, tmpFile, axios, log, patchConfig)
+  await log.info('Génération du schéma')
+  const datasetBase = {
+    isRest: true,
+    title: processingConfig.dataset.title,
+    primaryKey: [],
+    schema: []
+  }
+  for (const column of processingConfig.columns) {
+    if (column.isPrimaryKey) {
+      datasetBase.primaryKey.push(column.columnPath)
+    }
 
-  if (processingConfig.clearFiles) {
-    await fs.emptyDir(tmpDir)
+    const schema = {
+      key: column.columnPath.replace('.', ''),
+      type: column.columnType,
+      title: column.columnName ? column.columnName : column.columnPath
+    }
+    if (column.columnPath.includes('.')) {
+      schema['x-originalName'] = column.columnPath
+    }
+    datasetBase.schema.push(schema)
+  }
+
+  let dataset
+  if (processingConfig.datasetMode === 'create') {
+    await log.info('Création du jeu de données')
+    dataset = (await axios.post('api/v1/datasets', {
+      ...datasetBase,
+      id: processingConfig.dataset.id,
+      title: processingConfig.dataset.title,
+      extras: { processingId }
+    })).data
+    await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
+    await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
+  } else if (processingConfig.datasetMode === 'update') {
+    await log.info('Vérification du jeu de données')
+    dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
+    if (!dataset) throw new Error(`Le jeu de données n'existe pas, id${processingConfig.dataset.id}`)
+    await log.info(`Le jeu de donnée existe, id="${dataset.id}", title="${dataset.title}"`)
+  }
+
+  await log.step('Récupération, conversion et envoi des données')
+  const headers = { Accept: 'application/json' }
+  if (processingConfig.authorizationHeader) {
+    headers.Authorization = processingConfig.authorizationHeader
+  }
+
+  let nextPageURL = processingConfig.apiURL
+  while (nextPageURL) {
+    await log.info(`Récupération de ${nextPageURL}`)
+    const res = await axios({
+      method: 'get',
+      url: nextPageURL,
+      headers
+    })
+
+    let data = null
+    if (res && res.data) {
+      if (processingConfig.resultPath) {
+        data = getValueByPath(res.data, processingConfig.resultPath)
+        if (!data) {
+          throw new Error(`Le chemin ${processingConfig.resultPath} n'existe pas dans la réponse de l'API`)
+        }
+      } else {
+        data = res.data
+      }
+      if (!Array.isArray(data)) {
+        data = [data]
+        await log.warning('Le résultat de l\'API n\'est pas un tableau.')
+      }
+      if (processingConfig.nextPagePath) {
+        nextPageURL = getValueByPath(res.data, processingConfig.nextPagePath)
+      } else {
+        nextPageURL = null
+      }
+    }
+
+    if (data) {
+      await log.info('Conversion des données')
+      const formattedLines = []
+      for (const row of data) {
+        const formattedRow = {}
+        for (const column of processingConfig.columns) {
+          const path = column.columnPath
+          const value = getValueByPath(row, path)
+          if (column.multivalued && Array.isArray(value)) {
+            const values = []
+            for (const v of value) {
+              values.push(v)
+            }
+            formattedRow[path] = values.join(';')
+          } else if (value) {
+            formattedRow[path] = value
+          } else {
+            formattedRow[path] = ''
+          }
+        }
+        formattedLines.push(formattedRow)
+      }
+
+      if (formattedLines.length > 0) {
+        await log.info(`Envoi de ${formattedLines.length} lignes`)
+        await axios.post(`api/v1/datasets/${dataset.id}/_bulk_lines`, formattedLines)
+      }
+    }
   }
 }
--- test/test.js
+++ test/test.js
@@ -7,61 +7,64 @@ describe('test', function () {
   it('try', async function () {
     this.timeout(1000000)
 
-    // const context = testUtils.context({
-    //   pluginConfig: {},
-    //   processingConfig: {
-    //     datasetMode: 'create',
-    //     dataset: { title: 'Json mapping test' },
-    //     apiURL: 'https://www.data.gouv.fr/api/1/organizations/?page=1&page_size=100',
-    //     resultPath: 'data',
-    //     nextPagePath: 'next_page',
-    //     columns: [
-    //       {
-    //         columnPath: 'acronym',
-    //         columnName: 'Acronyme'
-    //       },
-    //       {
-    //         columnPath: 'logo',
-    //         columnName: 'Logo'
-    //       },
-    //       {
-    //         columnPath: 'badges',
-    //         columnName: 'Badges',
-    //         multivalued: true
-    //       },
-    //       {
-    //         columnPath: 'metrics.views',
-    //         columnName: 'Nombre de vues'
-    //       }
-    //     ],
-    //     clearFile: false
-    //   },
-    //   tmpDir: 'data'
-    // }, config, false)
-
     const context = testUtils.context({
       pluginConfig: {},
       processingConfig: {
         datasetMode: 'create',
         dataset: { title: 'Json mapping test' },
-        apiURL: 'https://api.insee.fr/metadonnees/V1/concepts/definitions',
-        resultPath: '',
-        nextPagePath: '',
+        apiURL: 'https://www.data.gouv.fr/api/1/organizations/?page=1&page_size=100',
+        resultPath: 'data',
+        nextPagePath: 'next_page',
         columns: [
           {
-            columnPath: 'id',
-            columnName: 'Identifiant'
+            columnPath: 'acronym',
+            columnName: 'Acronyme',
+            columnType: 'string'
           },
           {
-            columnPath: 'intitule',
-            columnName: 'Intitulé'
+            columnPath: 'logo',
+            columnName: 'Logo',
+            columnType: 'string'
+          },
+          {
+            columnPath: 'badges',
+            columnName: 'Badges',
+            columnType: 'object',
+            multivalued: true
+          },
+          {
+            columnPath: 'metrics.views',
+            columnName: 'Nombre de vues',
+            columnType: 'integer'
           }
-        ],
-        authorizationHeader: config.inseeAuthorizationHeader,
-        clearFile: false
+        ]
       },
       tmpDir: 'data'
     }, config, false)
+
+    // const context = testUtils.context({
+    //   pluginConfig: {},
+    //   processingConfig: {
+    //     datasetMode: 'create',
+    //     dataset: { title: 'Json mapping test' },
+    //     apiURL: 'https://api.insee.fr/metadonnees/V1/concepts/definitions',
+    //     resultPath: '',
+    //     nextPagePath: '',
+    //     columns: [
+    //       {
+    //         columnPath: 'id',
+    //         columnName: 'Identifiant'
+    //       },
+    //       {
+    //         columnPath: 'intitule',
+    //         columnName: 'Intitulé'
+    //       }
+    //     ],
+    //     authorizationHeader: config.inseeAuthorizationHeader,
+    //     clearFile: false
+    //   },
+    //   tmpDir: 'data'
+    // }, config, false)
 
     await processing.run(context)
   })
